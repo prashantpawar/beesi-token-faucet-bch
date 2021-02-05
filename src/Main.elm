88773e24f477e3a346b2c8fc6f5a71d847130365
Prashant Singh Pawar <prashantpawar@gmail.com>
2021-02-04 23:59:59 -0400

    - Created a Browser.sandbox

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,54 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Browser
+import Html exposing (Html, button, div, p, text)
+import Html.Events exposing (onClick)
 
 
 main =
-    text "Hello World"
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    Int
+
+
+init : Model
+init =
+    0
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Increment ] [ text "+" ]
+        , p [] [ text (String.fromInt model) ]
+        , button [ onClick Decrement ] [ text "-" ]
+        ]
